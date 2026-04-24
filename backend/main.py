b465452d782f013fa42eacba6390b0e4f5f02528
Sean Anderson <sean.wright.anderson@gmail.com>
2026-04-24 14:04:27 -0600

    Open CORS for all origins

--- backend/main.py
+++ backend/main.py
@@ -27,8 +27,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.origins_list,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
